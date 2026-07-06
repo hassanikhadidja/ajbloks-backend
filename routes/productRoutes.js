128ba67cbab72ecc34cb29ae21641c7c30f3ac3e
hassanikhadidja <khadidja.khaoila.hassani@gmail.com>
2026-07-06 19:53:55 +0100

    color

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,16 +1,23 @@
-const express  = require('express');
-const router   = express.Router();
+const express = require("express");
+const router = express.Router();
 const controlles = require("../controlles/productcontrolles");
-const upload     = require("../utils/multer");
-const { Auth }   = require('../middlewares/isAuth');
-const isAdmin    = require('../middlewares/isAdmin');
+const upload = require("../utils/multer");
+const { Auth } = require("../middlewares/isAuth");
+const isAdmin = require("../middlewares/isAdmin");
 
-router.get('/',      controlles.GetProducts);
-router.get("/:id",   controlles.GetOneProduct);
+function maybeMultipart(req, res, next) {
+  const contentType = String(req.headers["content-type"] || "");
+  if (contentType.includes("multipart/form-data")) {
+    return upload.array("files", 10)(req, res, next);
+  }
+  return next();
+}
 
-// upload.array("files", 10) → accepts up to 10 images under field name "files"
-router.post("/",     upload.array("files", 10), Auth, isAdmin, controlles.AddProduct);
-router.patch("/:id", upload.array("files", 10), Auth, isAdmin, controlles.UpdateProduct);
+router.get("/", controlles.GetProducts);
+router.get("/:id", controlles.GetOneProduct);
+
+router.post("/", maybeMultipart, Auth, isAdmin, controlles.AddProduct);
+router.patch("/:id", maybeMultipart, Auth, isAdmin, controlles.UpdateProduct);
 router.delete("/:id", Auth, isAdmin, controlles.DeleteProduct);
 
 module.exports = router;
